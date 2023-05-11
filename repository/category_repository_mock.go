0a25789f4e3708b5ba267dd695556d9c675c651f
firman196 <firmanagussaputro970@gmail.com>
2023-05-11 21:08:53 +0700

    add mock repository

---
+++ repository/category_repository_mock.go
@@ -0,0 +1,45 @@
+package repository
+
+import (
+	"golang-store/models/entity"
+
+	mock "github.com/stretchr/testify/mock"
+)
+
+type CategoryRepositoryMock struct {
+	mock mock.Mock
+}
+
+// Mock create a new category
+func (r *CategoryRepositoryMock) Create(category entity.Category) (*entity.Category, error) {
+	args := r.mock.Called(category)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	} else {
+		category := args.Get(0).(category)
+		return &category
+	}
+	return &category, nil
+}
+
+// Mock update a category
+func (r *CategoryRepositoryMock) Update(category entity.Category) (*entity.Category, error) {
+	args := r.mock.Called(category)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	} else {
+		result := args.Get(0).(category)
+		return &result, nil
+	}
+}
+
+// find category by id
+func (r *CategoryRepositoryMock) FindByID(id string) (*entity.Category, error) {
+	args := r.mock.Called(id)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	} else {
+		result := args.Get(0).(category)
+		return &result, nil
+	}
+}
